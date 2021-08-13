88087c4960cd864598f30d2252017fa46ccc02b0
gklpvi <gklpvi@me.com>
2021-08-13 13:53:42 +0300

    added comments

--- main.cpp
+++ main.cpp
@@ -12,11 +12,8 @@ class ringWrapper{
 
 public:
   ringWrapper(char* name, unsigned int count, int socket_id, unsigned int flags )
-  {myRing = rte_ring_create(name, count, socket_id, flags);};
-  ~ringWrapper(){rte_ring_free(myRing);};
-
-  //ringWrapper(rte_ring &);
-  //ring* operator=(){};
+  {myRing = rte_ring_create(name, count, socket_id, flags);};  //contructor using create method
+  ~ringWrapper(){rte_ring_free(myRing);};  // destructor using free method
 
   void enqueue(void* obj){rte_ring_enqueue(myRing, obj);};
   int dequeue(void** objPtr){return rte_ring_dequeue(myRing, objPtr);};
@@ -26,10 +23,10 @@ public:
 
 int main(int argc, char** argv)
 {
-    int ret = rte_eal_init(argc, argv);
+    int ret = rte_eal_init(argc, argv);  // to verify whether dpdk is linked correctly
     if (ret < 0)
 		rte_panic("Cannot init EAL\n");
 
-  //ringWrapper temp(g, );
+  //ringWrapper temp(g, );  // to try ring wrapper
     return 0;
 }
