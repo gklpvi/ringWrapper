123f0e172e953a1aeceb9e962d97d1e32b58e2ac
gklpvi <gklpvi@me.com>
2021-08-17 11:39:50 +0300

    enq and deq are working but not sure correctly

--- main.cpp
+++ main.cpp
@@ -14,36 +14,35 @@ public:
   {myRing = rte_ring_create(name, count, socket_id, flags);};  //contructor using create method
   ~ringWrapper(){rte_ring_free(myRing);};  // destructor using free method
 
-  void enqueue(void* obj){rte_ring_enqueue(myRing, obj);};
+  int enqueue(void* obj){return rte_ring_enqueue(myRing, obj);};
   int dequeue(void** objPtr){return rte_ring_dequeue(myRing, objPtr);};
   
   rte_ring* getPtr(){return myRing;};
-
 };
-
 
 int main(int argc, char** argv)
 {
     int ret = rte_eal_init(argc, argv);  // to verify whether dpdk is linked correctly
-    if (ret < 0)
-		  rte_panic("Cannot init EAL\n");
+    if (ret < 0)		  rte_panic("Cannot init EAL\n");
     
     char name[3];
     name[0] = 'g';
     name[1] = 'k';
     name[2] = '\0';
 
-    ringWrapper temp(name, 1, 0, RING_F_SP_ENQ);  // to try ring wrapper
+    ringWrapper temp(name, 32, 0, RING_F_SP_ENQ);  // to try ring wrapper
 
     cout << temp.getPtr() <<endl;
     cout <<"full: " << rte_ring_full(temp.getPtr()) << " -  empty: " <<  rte_ring_empty(temp.getPtr()) <<endl;
 
     int x = 5;
     void* ptrInt = &x;
-    temp.enqueue(ptrInt);
+    cout << "Enq: " << temp.enqueue(ptrInt) << endl;
     cout <<"full: " << rte_ring_full(temp.getPtr()) << " -  empty: " <<  rte_ring_empty(temp.getPtr()) <<endl;
 
-    void** deq;
-    cout << temp.dequeue(deq) << endl;
+    void* deq;
+    cout << temp.dequeue(&deq) << endl;
+    //cout << *deq << endl;
+
     return 0;
 }
